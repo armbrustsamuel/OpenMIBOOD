0d8fc15f4ca65c2a09e2102b90c7150d064ce789
Samuel Armbrust <samuel.armbrust@sap.com>
2025-06-03 18:43:40 -0300

    changing weights

--- openood/postprocessors/autoencoder_postprocessor.py
+++ openood/postprocessors/autoencoder_postprocessor.py
@@ -164,6 +164,7 @@ class AutoencoderPostprocessor(BasePostprocessor):
                 
                 # For reporting average loss:
                 avg_score = scores.mean().item()
+                print("Average Perceptual Loss:", avg_score)
 
                 # scores = torch.mean((data - reconstructed) ** 2, dim=(1, 2, 3))
 
@@ -171,8 +172,7 @@ class AutoencoderPostprocessor(BasePostprocessor):
                 # all_scores.append(np.atleast_1d(scores.cpu().numpy()))
 
                 # all_scores.append(scores.cpu().numpy().reshape(-1))
-                # all_scores.append(avg_score.cpu().detach().numpy())
-                all_scores.append(avg_score)
+                all_scores.append(avg_score.cpu().detach().numpy())
                 
                 # all_labels.append(labels)
                 # all_labels.append(labels.cpu().numpy().reshape(-1))
