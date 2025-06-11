8d0ad70252f7ecf965df934750cc89c17df5c7d8
Samuel Armbrust <samuel.armbrust@sap.com>
2025-06-10 22:53:59 -0300

    changing weights

--- openood/postprocessors/autoencoder_postprocessor.py
+++ openood/postprocessors/autoencoder_postprocessor.py
@@ -147,8 +147,7 @@ class AutoencoderPostprocessor(BasePostprocessor):
         # selected_layers = ['block1_conv2', 'block2_conv2', 'block3_conv4']
         # selected_layer_weights = [1.0, 0.75, 0.5]
 
-        # selected_layers = ['block2_conv2',"block3_conv3",'block4_conv3']
-        selected_layers = ['block1_conv2', 'block2_conv2', 'block3_conv3']
+        selected_layers = ['block2_conv2',"block3_conv3",'block4_conv3']
         selected_layer_weights = [8.0, 4.0, 2.0]
         # selected_layer_weights = [2.0 , 4.0 , 8.0]
 
