195d6b266f0eaf38608c8542912c6827fe80d983
Samuel Armbrust <samuel.armbrust@sap.com>
2025-06-17 16:23:37 -0300

    adaptations

--- scripts/download/OpenMIBOOD/download_midog_ccagt.py
+++ scripts/download/OpenMIBOOD/download_midog_ccagt.py
@@ -21,6 +21,7 @@ root = f'{script_dir}/../../../data/midog/far/ccagt_crops'
 if not os.path.exists(root):
     url = 'https://prod-dcd-datasets-cache-zipfiles.s3.eu-west-1.amazonaws.com/wg4bpm33hj-2.zip'
     # download_path = f'{script_dir}/tmp/ccagt.zip'
+    download_path = f'{download_path}/ccagt.zip'
     download_with_curl(url, download_path)
     with zipfile.ZipFile(download_path, 'r') as zip_file:
         zip_output = f'{script_dir}/tmp/ccagt'
